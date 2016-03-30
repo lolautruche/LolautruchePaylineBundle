01c41d6c58966838864f24c009f5d8037b9ca610
Jérôme Vieilledent <lolautruche@gmail.com>
2016-03-30 17:12:08 +0200

    Documented events in PHPDoc

--- Event/PaylineEvents.php
+++ Event/PaylineEvents.php
@@ -13,8 +13,23 @@ namespace Lolautruche\PaylineBundle\Event;
 
 final class PaylineEvents
 {
+    /**
+     * Triggered just before doWebPayment webservice is called by the Payline gateway.
+     * Listeners will receive a \Lolautruche\PaylineBundle\Event\WebTransactionEvent object.
+     *
+     * You may use this event to add extra parameters for the webservice.
+     */
     const PRE_WEB_TRANSACTION_INITIATE = 'payline.before_web_transaction_initiate';
+
+    /**
+     * Triggered after doWebPayment webservice was called by the Payline gateway.
+     * Listeners will receive a \Lolautruche\PaylineBundle\Event\ResultEvent object.
+     */
     const POST_WEB_TRANSACTION_INITIATE = 'payline.after_web_transaction_initiate';
 
+    /**
+     * Triggered just after getWebPaymentDetails webservice was called by the Payline gateway.
+     * Listeners will receive a \Lolautruche\PaylineBundle\Event\ResultEvent object.
+     */
     const WEB_TRANSACTION_VERIFY = 'payline.web_transaction_verify';
 }
