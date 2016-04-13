65ad90b2e96aa16ccae2a1ae7ac80b53c0b5f1c1
Jérôme Vieilledent <lolautruche@gmail.com>
2016-04-13 12:45:16 +0200

    Added tests for DIC extension + semantic config

---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+ * This file is part of the LolautruchePaylineBundle package.
+ *
+ * (c) Jérôme Vieilledent <jerome@vieilledent.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Lolautruche\PaylineBundle\Tests\DependencyInjection;
+
+use Lolautruche\PaylineBundle\DependencyInjection\Configuration;
+use Matthias\SymfonyConfigTest\PhpUnit\ConfigurationTestCaseTrait;
+use Payline\PaylineSDK;
+use PHPUnit_Framework_TestCase;
+
+class ConfigurationTest extends PHPUnit_Framework_TestCase
+{
+    use ConfigurationTestCaseTrait;
+
+    /**
+     * Return the instance of ConfigurationInterface that should be used by the
+     * Configuration-specific assertions in this test-case
+     *
+     * @return \Symfony\Component\Config\Definition\ConfigurationInterface
+     */
+    protected function getConfiguration()
+    {
+        return new Configuration();
+    }
+
+    /**
+     * @dataProvider currencySymbolProvider
+     */
+    public function testCurrencySymbols($currencySymbol, $expectedProcessedValue)
+    {
+        $minimalConfig = [
+            'merchant_id' => '123',
+            'access_key' => '456xyz789',
+            'contract_number' => '1234567',
+            'default_confirmation_route' => 'confirmation',
+            'default_error_route' => 'error',
+            'environment' => PaylineSDK::ENV_HOMO,
+        ];
+
+        $this->assertProcessedConfigurationEquals(
+            [['default_currency' => $currencySymbol] + $minimalConfig],
+            [
+                'default_currency' => $expectedProcessedValue,
+                'log_level' => 'warning',
+                'proxy' => [
+                    'host' => null,
+                    'port' => null,
+                    'login' => null,
+                    'password' => null,
+                ],
+            ] + $minimalConfig
+        );
+    }
+
+    public function currencySymbolProvider()
+    {
+        return [
+            ['€', 'EUR'],
+            ['$', 'DOLLAR'],
+            ['£', 'POUND'],
+        ];
+    }
+
+    /**
+     * @dataProvider lowercaseEnvironmentProvider
+     */
+    public function testLowercaseEnvironment($lowerCaseEnv, $expectedEnv)
+    {
+        $minimalConfig = [
+            'merchant_id' => '123',
+            'access_key' => '456xyz789',
+            'contract_number' => '1234567',
+            'default_confirmation_route' => 'confirmation',
+            'default_error_route' => 'error',
+            'default_currency' => 'EUR',
+        ];
+
+        $this->assertProcessedConfigurationEquals(
+            [['environment' => $lowerCaseEnv] + $minimalConfig],
+            [
+                'environment' => $expectedEnv,
+                'log_level' => 'warning',
+                'proxy' => [
+                    'host' => null,
+                    'port' => null,
+                    'login' => null,
+                    'password' => null,
+                ],
+            ] + $minimalConfig
+        );
+    }
+
+    public function lowercaseEnvironmentProvider()
+    {
+        return [
+            [strtolower(PaylineSDK::ENV_HOMO), PaylineSDK::ENV_HOMO],
+            [strtolower(PaylineSDK::ENV_DEV), PaylineSDK::ENV_DEV],
+            [strtolower(PaylineSDK::ENV_INT), PaylineSDK::ENV_INT],
+            [strtolower(PaylineSDK::ENV_PROD), PaylineSDK::ENV_PROD],
+        ];
+    }
+}
---
+++ Tests/DependencyInjection/LolautruchePaylineExtensionTest.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+ * This file is part of the LolautruchePaylineBundle package.
+ *
+ * (c) Jérôme Vieilledent <jerome@vieilledent.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Lolautruche\PaylineBundle\Tests\DependencyInjection;
+
+use Lolautruche\PaylineBundle\DependencyInjection\LolautruchePaylineExtension;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use Payline\PaylineSDK;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+class LolautruchePaylineExtensionTest extends AbstractExtensionTestCase
+{
+    /**
+     * Return an array of container extensions you need to be registered for each test (usually just the container
+     * extension you are testing.
+     *
+     * @return ExtensionInterface[]
+     */
+    protected function getContainerExtensions()
+    {
+        return [new LolautruchePaylineExtension()];
+    }
+
+    /**
+     * @dataProvider basicConfigProvider
+     */
+    public function testBasicConfig($currency, $logLevel = null)
+    {
+        $merchantId = '123';
+        $accessKey = '456xyz789';
+        $contractNumber = '1234567';
+        $environment = PaylineSDK::ENV_HOMO;
+        $confirmationRoute = 'confirmation';
+        $errorRoute = 'error';
+        $logLevel = $logLevel ?: 'warning';
+        $config = [
+            'merchant_id' => $merchantId,
+            'access_key' => $accessKey,
+            'contract_number' => $contractNumber,
+            'environment' => $environment,
+            'default_confirmation_route' => $confirmationRoute,
+            'default_error_route' => $errorRoute,
+            'default_currency' => $currency,
+            'log_level' => $logLevel,
+        ];
+
+        $this->load($config);
+        $this->assertContainerBuilderHasParameter('lolautruche_payline.merchant_id', $merchantId);
+        $this->assertContainerBuilderHasParameter('lolautruche_payline.access_key', $accessKey);
+        $this->assertContainerBuilderHasParameter('lolautruche_payline.default_contract_number', $contractNumber);
+        $this->assertContainerBuilderHasParameter(
+            'lolautruche_payline.default_currency',
+            constant('Lolautruche\PaylineBundle\Payline\WebTransaction::CURRENCY_'.$currency)
+        );
+        $this->assertContainerBuilderHasParameter('lolautruche_payline.environment', $environment);
+        $this->assertContainerBuilderHasParameter('lolautruche_payline.log_verbosity', constant('Monolog\Logger::'.strtoupper($logLevel)));
+        $this->assertContainerBuilderHasParameter('lolautruche_payline.default_confirmation_route', $confirmationRoute);
+        $this->assertContainerBuilderHasParameter('lolautruche_payline.default_error_route', $errorRoute);
+        $this->assertContainerBuilderHasParameter('lolautruche_payline.proxy.host', null);
+        $this->assertContainerBuilderHasParameter('lolautruche_payline.proxy.port', null);
+        $this->assertContainerBuilderHasParameter('lolautruche_payline.proxy.login', null);
+        $this->assertContainerBuilderHasParameter('lolautruche_payline.proxy.password', null);
+    }
+
+    public function basicConfigProvider()
+    {
+        return [
+            ['EUR', null],
+            ['EUR', 'info'],
+            ['DOLLAR', 'debug'],
+            ['CAD', 'notice'],
+            ['CHF', 'error'],
+            ['POUND', 'emergency'],
+        ];
+    }
+}
